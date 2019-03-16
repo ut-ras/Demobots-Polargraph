a0db00d54edea69af3845bbdddee776329f9a2ce
Cole Thompson <colethompson@utexas.edu>
2019-03-15 19:57:07 -0500

    FreeRTOS multithread example running, can new send commands while drawing!

--- PolargraphESP32/PolargraphESP32.ino
+++ PolargraphESP32/PolargraphESP32.ino
@@ -3,8 +3,6 @@
  *
  *
  * TODO
- * Use FreeRTOS Scheduler
- * Use a fixed size circular array for the coordinate buffer
  * Improve front end
  *
  * Scheduler https://docs.espressif.com/projects/esp-idf/en/latest/api-guides/freertos-smp.html
@@ -16,80 +14,95 @@
 #include <Arduino.h>
 #include <esp_task_wdt.h>
 
-
 #include "src/iot/webserver.h"
 #include "src/iot/wifi_setup.h"
-
 #include "src/polargraph/polargraph.h"
 
-long next_update = 0;
+
+// Macro for printing debug information, quickly turn prints on/ off
+#define DEBUG_MODE 0
+#if DEBUG_MODE
+  #define debugPrint(s) Serial.println(s)
+#else
+  #define debugPrint(s)
+#endif
 
 
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(115200);
 
-  setupWiFiAuto(ssid, pass);
-  //setupWiFi(WIFI_MODE, ssid, pass);   //Access Point or Station
+  //setupWiFiAuto(ssid, pass);
+  setupWiFi(WIFI_MODE, ssid, pass);   //Access Point or Station
   setupWebServer();                   //Set up the Web Server
-  next_update = millis();
+  setupPolargraph();                  //Set up the Polargraph
 
-  //Serial.println("WiFi mode=" + String(WIFI_MODE) + ", ssid = " + String(ssid) + ", pass = " + String(pass));
-  //Serial.println("Web server at " + webServerPath);
+  //debugPrint("WiFi mode=" + String(WIFI_MODE) + ", ssid = " + String(ssid) + ", pass = " + String(pass));
+  //debugPrint("Web server at " + webServerPath);
 
-  //setupPolargraph();
+  xTaskCreatePinnedToCore(&vPolargraphTaskCode,"polargraphTask", 2000, NULL, 2, NULL, 1);
+  xTaskCreatePinnedToCore(&vWebserverTaskCode,"webserverTask", 2000, NULL, 2, NULL, 1);
 
-  xTaskCreatePinnedToCore(&vPolargraphTaskCode,"polargraphTask",2000,(void*)1,4,NULL, 1);
-  xTaskCreatePinnedToCore(&vWebserverTaskCode,"webserverTask",2000,(void*)1,5,NULL, 1);
-
-
+  //remove the main arduino loop thread
+  vTaskDelete(NULL);
 }
 
 void loop() {
-  Serial.println("loop start");
-  //draw a line between two points if available/ enabled
-  //returns true if at a good stopping point to check webserver
-  /*if (loopPolargraph()) {
-    long t_curr = millis();
-    if (t_curr > next_update) {
-      loopWebServer();    //check the web server for updates
-      next_update = t_curr + MIN_UPDATE_MS;
-    }
-  }*/
+  //this thread has been stopped in setup()
 
-  delay(2000);
-  //vTaskSwitchContext();
-
-
-  //Serial.println("loop end");
+  debugPrint("loop start");
+  delay(1000);
+  debugPrint("loop end");
+  delay(1000);
 }
 
-void vPolargraphTaskCode(void * pvParameters){
-  Serial.println("vPolargraphTaskCode start");
-  configASSERT( ( (uint32_t) pvParameters) == 1);
-  for(;;){
-    esp_task_wdt_reset();
 
-      if (loopPolargraph()) {
-        delay(500);
-        //vTaskSwitchContext();
-      }
+
+void vPolargraphTaskCode(void * pvParameters){
+  debugPrint("vPolargraphTaskCode start");
+  while(true) {
+    esp_task_wdt_reset();
+    debugPrint("pg start");
+
+    if (loopPolargraph()) {
+      delay(500);
+    }
+
+    debugPrint("pg end");
   }
 }
 
 void vWebserverTaskCode(void * pvParameters){
-  Serial.println("vWebserverTaskCode start");
-  configASSERT( ( (uint32_t) pvParameters) == 1);
-  for(;;){
+  debugPrint("vWebserverTaskCode start");
+  static long next_update = millis();
+
+  while(true) {
     esp_task_wdt_reset();
+    debugPrint("ws start");
 
     long t_curr = millis();
     if (t_curr > next_update) {
       loopWebServer();
       next_update = t_curr + MIN_UPDATE_MS;
     }else  {
-      delay(500);
+      delay(20);
     }
 
+    debugPrint("ws end");
+  }
+}
+
+
+// Single thread old version
+void singleThreadLoop() {
+  //draw a line between two points if available/ enabled
+  //returns true if at a good stopping point to check webserver
+  static long next_update = millis();
+  if (loopPolargraph()) {   //returns true if the polargraph is at a stopping point
+    long t_curr = millis();
+    if (t_curr > next_update) {
+      loopWebServer();    //check the web server for updates
+      next_update = t_curr + MIN_UPDATE_MS;
+    }
   }
 }
