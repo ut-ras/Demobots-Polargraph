6365ed17670cf5c9a59d679e7cd45b8143031d0d
chadharthan <32985726+chadharthan@users.noreply.github.com>
2019-04-05 17:49:44 -0500

    Better string parsing

--- Createathon2019/PolargraphESP32_Async/src/iot/json_parse.h
+++ Createathon2019/PolargraphESP32_Async/src/iot/json_parse.h
@@ -2,58 +2,21 @@
 #define JSON_PARSE_H__
 
 #include <Arduino.h>
+#include <stdlib.h>
+#include <stdio.h>
 
-/* TODO use char * and c string lib functions  */
+typedef struct json_obj {
+  char * attr;
+  char * value;
+} json_obj_t;
+
+void startJSONParse(char * json, json_obj_t* _ret);
+void getNextValue();
+String getFromJson(String json, String attr_name, String notFoundVal);
+String getFromJson(String json, String attr_name);
+String getFromJson2(String json, String attr_name, String notFoundVal);
+bool stringToBool(String b);
 
 
-
-// This isn't a great JSON parser but it works for now
-// assumes no duplicate names
-// finds this format (must be exactly this, with the comma): 'attr_name':'val',
-String getFromJson(String json, String attr_name, String notFoundVal) {
-  //String value = notFoundVal;
-  String quot = "'";
-  int index = json.indexOf("'" + attr_name + "'");
-  if (index == -1) {
-    index = json.indexOf("\"" + attr_name + "\"");
-    quot = "\"";
-  }
-  if (index > -1) {
-    index += String("'" + attr_name + "'").length();
-    int start_index = json.indexOf(quot, index) + 1;
-    return json.substring(start_index, json.indexOf(quot, start_index));
-    //Serial.println("JSON parse | " + attr_name + ": " + value);
-  }
-  return notFoundVal;
-}
-
-String getFromJson(String json, String attr_name) {
-  return getFromJson(json, attr_name, "");
-}
-
-String getFromJson2(String json, String attr_name, String notFoundVal) {
-  //String value = notFoundVal;
-  String quot = "'";
-  int index = json.indexOf("'" + attr_name + "'");
-  if (index == -1) {
-    index = json.indexOf("\"" + attr_name + "\"");
-    quot = "\"";
-  }
-  if (index > -1) {
-    index += String("'" + attr_name + "'").length();
-    int start_index = json.indexOf(quot, index) + 1;
-    json = json.substring(start_index, json.indexOf(quot, start_index));
-    //Serial.println("JSON parse | " + attr_name + ": " + value);
-  }
-  else {
-    json = notFoundVal;
-  }
-  return json;
-}
-
-bool stringToBool(String b) {
-  if (b[0] == 't' || b[0] == 'T') { return true; }
-  else { return false; }
-}
 
 #endif
